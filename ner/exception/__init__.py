e21a4078645b6f8a23de63794b34cc14516952e2
Rahul-404 <rahulshelke981@gmail.com>
2025-06-09 10:50:40 +0530

    added: implement custom exception handling and logging setup

---
+++ ner/exception/__init__.py
@@ -0,0 +1,22 @@
+import sys
+
+class CustomeException(Exception):
+
+    def __init__(self, error_message: Exception, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = CustomeException.get_detailed_error_message(error_message=error_message,
+                                                                         error_detail=error_detail)
+        
+    def get_detailed_error_message(error_message: Exception, error_detail: sys) -> str:
+        _, _, exec_tb = error_detail.exc_info()
+        try_block_line_number = exec_tb.tb_lineno
+        file_name = exec_tb.tb_frame.f_code.co_filename
+        error_message = f"Error occurred in script: [ {file_name} ] at line number : [ {try_block_line_number} ] error "\
+                        f"message: [ {error_message} ]"
+        return error_message
+    
+    def __str__(self):
+        return self.error_message
+    
+    def __repr__(self) -> str:
+        return str(CustomeException.__name__)
---
+++ ner/logger/__init__.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+import logging
+from datetime import dateti"ner"D
+IR = "logs"
+TIMESTAMP = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+def get_log_filename()
+    file_name = f"log_{TIMESTAMP}.log"
+    return file_name
+
+LOG_FILENAME = get_log_filename()
+
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILENAME)
+
+logging.basicConfig(
+    logging=logging.INFO,
+    filemode="w",
+    format="[%(asctime)s] \t%(levelname)s \t%(lineno)d  \t%(filename)s \t%(funcName)s() \t%(message)s",
+    filename=LOG_FILE_PATH
+)
+
+logger = logging.getLogger("ner")
